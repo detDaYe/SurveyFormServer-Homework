e12bf3b230d8617714464d59bc51c64f53046286
detDaYe <mindaye8504@gmail.com>
2021-04-26 22:12:02 +0900

    init-2

---
+++ src/main/java/org/lumenk/object/webserver/WebserverApplication.java
@@ -0,0 +1,18 @@
+package org.lumenk.object.webserver;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserverApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebserverApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
