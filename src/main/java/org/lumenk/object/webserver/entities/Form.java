a7dd720325d9f2f69d245f473d6f3ee65bbbd7ad
detDaYe <mindaye8504@gmail.com>
2021-04-30 22:37:38 +0900

    ｅｄｉｔ

--- src/main/java/org/lumenk/object/webserver/entities/Form.java
+++ src/main/java/org/lumenk/object/webserver/entities/Form.java
@@ -1,4 +1,26 @@
 package org.lumenk.object.webserver.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Form {
+
+    @Id
+    private Long id;
+
+    public Form(){}
+
+    public Form(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
 }
