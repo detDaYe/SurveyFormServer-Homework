7f727c08ebc5448f1946d404d13d7bb71c392ba8
detDaYe <mindaye8504@gmail.com>
2021-05-07 14:55:34 +0900

    CRUD draft complete

--- src/main/java/org/lumenk/object/webserver/controllers/api/UserCRUDAPI.java
+++ src/main/java/org/lumenk/object/webserver/controllers/api/UserCRUDAPI.java
@@ -1,4 +1,56 @@
 package org.lumenk.object.webserver.controllers.api;
 
+import org.lumenk.object.webserver.entities.User;
+import org.lumenk.object.webserver.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
 public class UserCRUDAPI {
+
+    private final UserRepository userRepository;
+
+    public UserCRUDAPI(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @GetMapping("/api/user/get")
+    public ResponseEntity<User> readUserRequest(@RequestBody User user){
+        Optional<User> optionalUser = userRepository.findById(user.getId());
+        if(optionalUser.isEmpty())
+            return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<User>(optionalUser.get(), HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/api/user/add")
+    public ResponseEntity addUserRequest(@RequestBody User user){
+        Optional<User> optionalUser = userRepository.findById(user.getId());
+        if(optionalUser.isPresent())
+            return new ResponseEntity("user already exists", HttpStatus.BAD_REQUEST);
+        userRepository.save(user);
+        return new ResponseEntity("sucess", HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/api/user/update")
+    public ResponseEntity updateUserRequest(@RequestBody User user){
+        Optional<User> optionalUser = userRepository.findById(user.getId());
+        if(optionalUser.isEmpty())
+            return new ResponseEntity("user not exists", HttpStatus.BAD_REQUEST);
+        userRepository.save(user);
+        return new ResponseEntity("sucess", HttpStatus.ACCEPTED);
+    }
+
+    @PostMapping("/api/user/delete")
+    public ResponseEntity deleteUserRequest(@PathVariable String id){
+        Optional<User> user = userRepository.findById(id);
+
+        if(user.isPresent()){
+            userRepository.delete(user.get());
+            return new ResponseEntity("success", HttpStatus.ACCEPTED);
+        }else return new ResponseEntity("user not found", HttpStatus.NO_CONTENT);
+    }
 }
