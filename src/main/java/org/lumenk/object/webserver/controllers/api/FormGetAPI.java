1e70c746df5cdae07878cf8763101822d69c2697
detDaYe <mindaye8504@gmail.com>
2021-05-25 16:27:06 +0900

    Form Get API 분리됨

---
+++ src/main/java/org/lumenk/object/webserver/controllers/api/FormGetAPI.java
@@ -0,0 +1,32 @@
+package org.lumenk.object.webserver.controllers.api;
+
+import org.lumenk.object.webserver.entities.Form;
+import org.lumenk.object.webserver.entities.dto.FormDto;
+import org.lumenk.object.webserver.repositories.FormRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class FormGetAPI {
+
+    @Autowired
+    private FormRepository formRepository;
+
+    @GetMapping("/api/form/get/{id}")
+    public ResponseEntity<FormDto> getForm(@PathVariable("id") Long id){
+        if(null == id)
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Optional<Form> optionalForm = formRepository.findById(id);
+        if(optionalForm.isEmpty())
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(new FormDto(optionalForm.get()), HttpStatus.OK);
+    }
+}
