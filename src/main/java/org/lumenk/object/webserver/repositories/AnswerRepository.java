8f3a3bf0b227a218ee06976eb07c6bddccfd4863
detDaYe <mindaye8504@gmail.com>
2021-05-26 10:11:25 +0900

    Repository annotation 추가

--- src/main/java/org/lumenk/object/webserver/repositories/AnswerRepository.java
+++ src/main/java/org/lumenk/object/webserver/repositories/AnswerRepository.java
@@ -1,7 +1,11 @@
 package org.lumenk.object.webserver.repositories;
 
 import org.lumenk.object.webserver.entities.Answer;
+import org.lumenk.object.webserver.entities.Form;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
+
 }
