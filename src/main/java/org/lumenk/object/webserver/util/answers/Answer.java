d5dffb3deb3a640e00ac1ab47e82596e1a6c2429
detDaYe <mindaye8504@gmail.com>
2021-05-26 17:39:53 +0900

    Add fromJson methods

--- src/main/java/org/lumenk/object/webserver/util/answers/Answer.java
+++ src/main/java/org/lumenk/object/webserver/util/answers/Answer.java
@@ -1,5 +1,9 @@
 package org.lumenk.object.webserver.util.answers;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class Answer {
     public Answer(int questionNumber) {
         this.questionNumber = questionNumber;
@@ -9,5 +13,17 @@ public class Answer {
 
     public int getQuestionNumber() {
         return questionNumber;
+    }
+
+    private static final Gson gson = new Gson();
+    public static Answer fromJson(String jsonString){
+        JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+
+        if(jsonObject.has("choice"))
+            return gson.fromJson(jsonString, ChoiceAnswer.class);
+        else if(jsonObject.has("value"))
+            return gson.fromJson(jsonString, EssayAnswer.class);
+        else
+            return null;
     }
 }
--- src/main/java/org/lumenk/object/webserver/util/questions/Question.java
+++ src/main/java/org/lumenk/object/webserver/util/questions/Question.java
@@ -1,5 +1,9 @@
 package org.lumenk.object.webserver.util.questions;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class Question {
     protected int number;
     protected String title;
@@ -17,5 +21,19 @@ public class Question {
 
     public boolean isRequired() {
         return isRequired;
+    }
+
+
+    private static final Gson gson = new Gson();
+    public static Question fromJson(String jsonString){
+        JsonObject jsonObject = JsonParser.parseString(jsonString).getAsJsonObject();
+
+        if(jsonObject.has("choices"))
+            return gson.fromJson(jsonString, ChoiceTypeQuestion.class);
+        else if(jsonObject.has("maxLength"))
+            return gson.fromJson(jsonString, EssayTypeQuestion.class);
+        else
+            return null;
+
     }
 }
